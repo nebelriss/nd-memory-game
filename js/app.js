397f09cb9c808972781f84ade14e3d31b65216b1
Michel Heiniger <git@akiramail.com>
2018-06-22 08:09:26 +0200

    Added basic UI logic.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,117 @@
 /*
- * Create a list that holds all of your cards
+ * global vars
  */
+const cardFaces = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb'];
 
+const DOMStrings = {
+  'deck': '.deck',
+  'moves': '.moves',
+  'restart': '.restart'
+}
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+const classStrings = {
+  'card': 'card',
+  'fa': 'fa'
+}
+
+/* 
+ * UI functions
  */
+function updateMoves(num) {
+  document.querySelector(DOMStrings.moves).textContent = num;
+}
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+function clearGameBoard() {
+  deck = document.querySelector(DOMStrings.deck);
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (deck.firstChild) {
+    deck.removeChild(deck.firstChild);
+  }
+}
 
-    return array;
+function drawGameBoard(cards) {
+
+  // create a fragment
+  const fragment = document.createDocumentFragment();
+
+  for (let card of cards) {
+    // create li element and add card class
+    const li = document.createElement('li');
+    li.className = classStrings.card + ' card';
+
+    // create icon element and append to li
+    const icon = document.createElement('i');
+    icon.className = `${classStrings.fa} ${classStrings.fa}-${card}`;
+
+    li.appendChild(icon);
+
+    // append li to fragment
+    fragment.appendChild(li);
+  }
+  document.querySelector(DOMStrings.deck).appendChild(fragment);
 }
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * Game control functions
  */
+function gameController(e) {
+  e.target.classList.add('show');
+  e.target.classList.add('open');
+}
+
+
+/*
+ * Basic functions
+ */
+function setUpEventListeners() {
+  // listener for the cards
+  document.querySelector(DOMStrings.deck).addEventListener('click', gameController);
+
+  // listener for reset button
+  document.querySelector(DOMStrings.restart).addEventListener('click', initGame);
+}
+
+function createFullShuffledDeck(cards) {
+  const array = cards.concat(cards);
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+/*
+ * Init
+ */
+function initGame() {
+  // TODO: init or reset game state
+  updateMoves(0);
+
+  // duplicate array, combine it and shuffle it.
+  const fulldeck = createFullShuffledDeck(cardFaces);
+
+  // clear board
+  clearGameBoard();
+
+  // draw gameboard with fulldeck
+  drawGameBoard(fulldeck);
+}
+
+function init() {
+  // init game
+  initGame();
+
+  // add event listeners
+  setUpEventListeners();
+}
+
+init();
